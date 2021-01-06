4068f953d5357eeb61393ff0873957f00341688c
Micah R Ledbetter <me@micahrl.com>
2021-01-05 18:14:59 -0600

    Add mock data for posts/update

--- lib/Pinboard/FauxData/index.ts
+++ lib/Pinboard/FauxData/index.ts
@@ -1,12 +1,13 @@
 const ApiPostsAll = require('./mocks.api.posts.all.json');
 const ApiPostsGetMultiple = require('./mocks.api.posts.get.multiple.json');
 const ApiPostsRecent = require('./mocks.api.posts.recent.json');
+const ApiPostsUpdate = require('./mocks.api.posts.update');
 const ApiTagsGet = require('./mocks.api.tags.get.json');
 const FeedsUnauthenticatedPopular = require('./mocks.feeds.unauthenticated.popular');
 const FeedsUnauthenticatedRecent5 = require('./mocks.feeds.unauthenticated.recent.5.json');
 
 export const FauxApiData: {[key: string]: object} = {
-  'posts/update': {},
+  'posts/update': ApiPostsUpdate,
   'posts/add': {},
   'posts/delete': {},
   'posts/dates': {},
